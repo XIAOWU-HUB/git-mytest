28038674b3477c6a9c71faf7c1b7591143b62cfc
xiaowu <1045873679@qq.com>
2022-08-11 17:23:26 +0800

    Second commit

--- src/main/java/cn/wolfcode/test/GitTest.java
+++ src/main/java/cn/wolfcode/test/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git 17:41");
+        System.out.println("Hello Git 17:22");
+        System.out.println("Hello Git 17:23");
     }
 }
