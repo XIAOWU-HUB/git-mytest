bf87468be608c6faa929cfd361c0f135d7641502
XIAOWU-HUB <65882099+XIAOWU-HUB@users.noreply.github.com>
2022-08-12 00:10:54 +0800

    Update GitTest.java

--- src/main/java/cn/wolfcode/test/GitTest.java
+++ src/main/java/cn/wolfcode/test/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println(1111);
         System.out.println("12345678");
         System.out.println("push commit");
+        System.out.println("pull commit");
     }
 }
